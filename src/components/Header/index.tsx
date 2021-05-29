5cdde5114d495713c3f65eca97a011494968fa80
MoreTimeApps <moretimeapps@gmail.com>
2021-05-29 21:14:27 +0530

    Third Commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -340,7 +340,7 @@ export default function Header() {
           >
             {t('pool')}
           </StyledNavLink>
-          <StyledExternalLink id={`stake-nav-link`} href={'https://www.dextools.io/app/uniswap/pair-explorer/0xf82d8ec196fb0d56c6b82a8b1870f09502a49f88'}>
+          <StyledExternalLink id={`stake-nav-link`} href={'https://www.dextools.io/app/uniswap/pair-explorer/0xa7cc68376682ff7e861ac4e616486407f944e672'}>
             Chart <span style={{ fontSize: '11px' }}>↗</span>
           </StyledExternalLink>
         </HeaderLinks>
